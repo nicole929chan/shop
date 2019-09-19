7f8fd1a9afad0a210250c52511df4281575a6acd
Nicole <nicole929chan@gmail.com>
2019-09-19 11:31:49 +0800

    忘記密碼, WIP

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,30 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
-
-    use SendsPasswordResetEmails;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function sendResetLinkEmail(Request $request)
     {
-        $this->middleware('guest');
+        $attributes = $request->validate([
+            'email' => 'required|email|exists:users'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ Route::group(['prefix' => 'auth'], function() {
     Route::post('register', 'Auth\RegisterController@action');
     Route::post('login', 'Auth\LoginController@action');
     Route::get('me', 'Auth\MeController@action');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 });
 Route::post('logout', 'Auth\LogoutController@logout');
 
